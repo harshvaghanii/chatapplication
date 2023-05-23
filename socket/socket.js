7a01d3cfbd5b9b01b1fbae9c1986e1f2175289fe
Harsh Vaghani <harshvaghani98@gmail.com>
2023-05-23 13:42:47 +0530

    Adding realtime active status of user

--- socket/socket.js
+++ socket/socket.js
@@ -5,7 +5,7 @@ const io = require("socket.io")(8000, {
     },
 });
 
-const users = [];
+let users = [];
 const addUser = (userId, socketId, userInfo) => {
     const checkUser = users.some((user) => user.userId === userId);
     if (!checkUser) {
@@ -17,10 +17,18 @@ const addUser = (userId, socketId, userInfo) => {
     }
 };
 
+const removeUser = (socketId) => {
+    users = users.filter((user) => user.socketId !== socketId);
+};
+
 io.on("connection", (socket) => {
     console.log("Socket is connecting...");
     socket.on("addUser", (userId, userInfo) => {
         addUser(userId, socket.id, userInfo);
+        io.emit("getUser", users);
+    });
+    socket.on("disconnect", () => {
+        removeUser(socket.id);
         io.emit("getUser", users);
     });
 });
